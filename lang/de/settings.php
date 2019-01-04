313d638419385ec438ae10563ddda89d9ed1e85b
Braintelligence <Braintelligence@users.noreply.github.com>
2019-01-04 12:29:52 +0100

    German settings.php

---
+++ lang/de/settings.php
@@ -0,0 +1,8 @@
+<?php
+        $lang['namespaces'] = "Namespaces (komma-getrennt)";
+        $lang['notify_create'] = "Benachrichtigen sobald Seiten erstellt wurden";
+        $lang['notify_edit'] = "Benachrichtigen sobald Seiten bearbeitet wurden";
+        $lang['notify_delete'] = "Benachrichtigen sobald Seiten gelöscht wurden";
+        $lang['show_summary'] = "Zusammenfassung anzeigen, wenn verfügbar";
+        $lang['webhook']    = "Mattermost Incoming Webhook URL";
+        $lang['username']   = "Benutzername, den der Bot benutzen soll";
