c2ccc12755ca0e1809b2a8bd82e04f58cdfd22e2
Braintelligence <Braintelligence@users.noreply.github.com>
2019-01-04 12:27:37 +0100

    Default to root namespace

--- conf/default.php
+++ conf/default.php
@@ -1,5 +1,5 @@
 <?php
-        $conf['namespaces'] = "";
+        $conf['namespaces'] = ":";
         $conf['notify_create'] = 1;
         $conf['notify_edit'] = 1;
         $conf['notify_delete'] = 1;
